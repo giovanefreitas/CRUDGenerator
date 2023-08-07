4fd74017f78efaeb5ff7a67ba8d64e46f6209618
Giovane J. O. Freitas <gjofreitas@pbh.gov.br>
2023-08-07 08:17:00 -0300

    Criando listagem de projetos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,24 @@
 <template>
   <main>
     <h1>Seja bem-vindo!</h1>
+    <div v-for="project of projectList" :key="project._id" class="row">
+      <router-link :to="`/project/${project._id}`">{{ project.name }}</router-link>
+    </div>
   </main>
 </template>
 
 <script setup>
 import { nextTick, onMounted, ref } from 'vue'
 
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}`
+
+const projectList = ref([])
+
 onMounted(() => {
   fetch(`${BASE_URL}/cadastros/`)
     .then((resp) => resp.json())
     .then((dados) => {
-      baseForm.value = dados
-      selectedField.value = null
-      incializarSortable()
+      projectList.value = dados
     })
 })
 </script>
