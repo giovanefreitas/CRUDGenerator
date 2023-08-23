67735e52069e509f9b73da5f9554dd955e48d412
Giovane J. O. Freitas <gjofreitas@pbh.gov.br>
2023-08-23 17:38:51 -0300

    Redesenhando interface

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Toast from 'primevue/toast'
 </script>
 
 <template>
-  <header>
-    <div class="container">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/schema-importer">Importar</RouterLink>
-        <RouterLink to="/project/new">Novo projeto</RouterLink>
-      </nav>
-    </div>
-  </header>
   <Toast />
   <RouterView />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,83 @@
 <template>
   <main>
-    <h1>Seja bem-vindo!</h1>
-    <div v-for="project of projectList" :key="project.id" class="row">
-      <router-link :to="`/project/${project.id}`">{{ project.name }}</router-link>
-    </div>
+    <Menubar :model="items">
+      <template #start>
+        <img alt="logo" src="/src/assets/logo.svg" height="40" class="mr-2" />
+      </template>
+      <template #end>
+        <InputText placeholder="Pesquisar" type="text" />
+      </template>
+    </Menubar>
+    <DataTable :value="projectList" tableStyle="min-width: 50rem">
+      <Column field="name" header="Nome"></Column>
+      <Column field="description" header="Descrição"></Column>
+      <Column header="Status">
+        <template #body="slotProps">
+          <Tag
+            :value="slotProps.data.published ? 'Publicado' : 'Rascunho'"
+            :severity="getSeverity(slotProps.data)"
+          />
+        </template>
+      </Column>
+    </DataTable>
   </main>
 </template>
 
 <script setup>
-import { nextTick, onMounted, ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import Menubar from 'primevue/menubar'
+import InputText from 'primevue/inputtext'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import Tag from 'primevue/tag'
 
 const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}`
 
 const projectList = ref([])
 
+const items = ref([
+  {
+    label: 'Novo projeto',
+    icon: 'pi pi-fw pi-plus',
+    command: () => createNewProject()
+  },
+  {
+    label: 'Atualizar',
+    icon: 'pi pi-fw pi-refresh',
+    command: () => loadProjects()
+  },
+  {
+    label: 'Sair',
+    icon: 'pi pi-fw pi-power-off',
+    command: () => signout()
+  }
+])
+
 onMounted(() => {
+  loadProjects()
+})
+
+function loadProjects() {
   fetch(`${BASE_URL}/projects/`)
     .then((resp) => resp.json())
     .then((dados) => {
       projectList.value = dados
     })
-})
+}
+
+function createNewProject() {
+  alert('novo projeto')
+}
+
+function signout() {
+  alert('Saiu.')
+}
+
+const getSeverity = (project) => {
+  if (project.published) {
+    return 'success'
+  } else {
+    return 'warning'
+  }
+}
 </script>
